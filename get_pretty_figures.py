daf1a72b5d59bc680fe1a5cbad549e47f63d54dc
Jaerin Lee <ironjr@snu.ac.kr>
2023-11-26 21:06:19 +0900

    pretty figures for not so pretty work

---
+++ get_pretty_figures.py
@@ -0,0 +1,160 @@
+import os
+import glob
+
+import networkx as nx
+import graph_tool as gt
+from graph_tool.topology import topological_sort
+from graph_tool.draw import graph_draw, radial_tree_layout, sfdp_layout
+import dagviz
+
+import torch
+
+
+def get_prop_type(value, key=None):
+    """
+    Performs typing and value conversion for the graph_tool PropertyMap class.
+    If a key is provided, it also ensures the key is in a format that can be
+    used with the PropertyMap. Returns a tuple, (type name, value, key)
+    """
+    if isinstance(key, unicode):
+        # Encode the key as ASCII
+        key = key.encode('ascii', errors='replace')
+
+    # Deal with the value
+    if isinstance(value, bool):
+        tname = 'bool'
+
+    elif isinstance(value, int):
+        tname = 'float'
+        value = float(value)
+
+    elif isinstance(value, float):
+        tname = 'float'
+
+    elif isinstance(value, unicode):
+        tname = 'string'
+        value = value.encode('ascii', errors='replace')
+
+    elif isinstance(value, dict):
+        tname = 'object'
+
+    else:
+        tname = 'string'
+        value = str(value)
+
+    return tname, value, key
+
+
+def nx2gt(nxG):
+    """
+    Converts a networkx graph to a graph-tool graph.
+    """
+    # Phase 0: Create a directed or undirected graph-tool Graph
+    gtG = gt.Graph(directed=nxG.is_directed())
+
+    # Add the Graph properties as "internal properties"
+    for key, value in nxG.graph.items():
+        # Convert the value and key into a type for graph-tool
+        tname, value, key = get_prop_type(value, key)
+
+        prop = gtG.new_graph_property(tname) # Create the PropertyMap
+        gtG.graph_properties[key] = prop     # Set the PropertyMap
+        gtG.graph_properties[key] = value    # Set the actual value
+
+    # Phase 1: Add the vertex and edge property maps
+    # Go through all nodes and edges and add seen properties
+    # Add the node properties first
+    nprops = set() # cache keys to only add properties once
+    for node, data in nxG.nodes(data=True):
+
+        # Go through all the properties if not seen and add them.
+        for key, val in data.items():
+            if key in nprops: continue # Skip properties already added
+
+            # Convert the value and key into a type for graph-tool
+            tname, _, key  = get_prop_type(val, key)
+
+            prop = gtG.new_vertex_property(tname) # Create the PropertyMap
+            gtG.vertex_properties[key] = prop     # Set the PropertyMap
+
+            # Add the key to the already seen properties
+            nprops.add(key)
+
+    # Also add the node id: in NetworkX a node can be any hashable type, but
+    # in graph-tool node are defined as indices. So we capture any strings
+    # in a special PropertyMap called 'id' -- modify as needed!
+    gtG.vertex_properties['id'] = gtG.new_vertex_property('string')
+
+    # Add the edge properties second
+    eprops = set() # cache keys to only add properties once
+    for src, dst, data in nxG.edges(data=True):
+
+        # Go through all the edge properties if not seen and add them.
+        for key, val in data.items():
+            if key in eprops: continue # Skip properties already added
+
+            # Convert the value and key into a type for graph-tool
+            tname, _, key = get_prop_type(val, key)
+
+            prop = gtG.new_edge_property(tname) # Create the PropertyMap
+            gtG.edge_properties[key] = prop     # Set the PropertyMap
+
+            # Add the key to the already seen properties
+            eprops.add(key)
+
+    # Phase 2: Actually add all the nodes and vertices with their properties
+    # Add the nodes
+    vertices = {} # vertex mapping for tracking edges later
+    for node, data in nxG.nodes(data=True):
+
+        # Create the vertex and annotate for our edges later
+        v = gtG.add_vertex()
+        vertices[node] = v
+
+        # Set the vertex properties, not forgetting the id property
+        data['id'] = str(node)
+        for key, value in data.items():
+            gtG.vp[key][v] = value # vp is short for vertex_properties
+
+    # Add the edges
+    for src, dst, data in nxG.edges(data=True):
+
+        # Look up the vertex structs from our vertices mapping and add edge.
+        e = gtG.add_edge(vertices[src], vertices[dst])
+
+        # Add the edge properties
+        for key, value in data.items():
+            gtG.ep[key][e] = value # ep is short for edge_properties
+
+    # Done, finally!
+    return gtG
+
+
+root = 'checkpoint/v18-mes-dr5-do0-lr2-cma'
+outdir = 'draw'
+
+graphs = []
+files = glob.glob(os.path.join(root, '*.pth'))
+for path in files:
+    fname, _ = os.path.splitext(os.path.basename(path))
+    epoch = int(fname[9:])
+    state = torch.load(path, map_location='cpu')
+    G, nodemap = state['graphs']
+
+    #  gtG = nx2gt(G[-1])
+    #  #  pos = radial_tree_layout(gtG, gtG.vertex(0))
+    #  deg = gtG.degree_property_map("in")
+    #  order = gtG.new_vertex_property("int")
+    #  order.a = topological_sort(gtG)
+    #  pos = sfdp_layout(gtG)
+    #
+    #  graph_draw(
+    #      gtG, pos=pos, vertex_text=gtG.vertex_index, output=os.path.join(outdir, f'{epoch:04d}.png'),
+    #      vorder=order,
+    #  )
+    #  state = minimize_nested_blockmodel_dl(gtG)
+    #  draw_hierarchy(state, output=os.path.join(outdir, f'{epoch}.png'))
+
+    r = dagviz.render_svg(G[-1])
+    with open(os.path.join(outdir, f'{epoch}.svg'), 'wt') as fs:
+        fs.write(r)
